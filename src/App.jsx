e26cc33fa35b0388b0510bfaf7641937132851be
GADOBUA <emmanuelobua54@gmail.com>
2024-05-10 13:33:29 +0300

    Add header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 import DoctorRegistration from './assets/Pages/DoctorRegistration';
 import PatientRegistration from './assets/Pages/PatientRegistration';
 import Login from './assets/Pages/Login';
+import Home from './assets/Pages/Home';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
     <Route path='/Login' element={ <Login /> }/>
     <Route path='/PatientRegistration' element={ <PatientRegistration /> }/>
     <Route path='/DoctorRegistration' element={ <DoctorRegistration /> }/>
+    <Route path='/' element={<Home/>}/>
     
     </Routes>
     </BrowserRouter>
--- src/assets/Pages/Home.jsx
+++ src/assets/Pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../Components/Header'
+
+
+
 
 function Home() {
   return (
     <div>
-      
+    <Header/>
     </div>
   )
 }
