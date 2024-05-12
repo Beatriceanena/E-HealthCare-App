cf613e3b651aedc100bb2d3631843b0713a874b1
GADOBUA <emmanuelobua54@gmail.com>
2024-05-12 14:33:58 +0300

    edit the footer

--- src/assets/Components/Footer.jsx
+++ src/assets/Components/Footer.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 import './Footer.css'
 function Footer(props) {
   return (
-    <div>
-      <div className='footer'>
-      <div className="contact">
-      <h5>Contact Info</h5>
-      <a href="e-mail" type='e-mail' style={{color:'white'}}>ehealthcare@gmail.com</a>
-      <a href="number" style={{color:'white'}}>+256 758903837</a>
-      <a href="number" style={{color:'white'}}>+256 771629949</a>
-      </div>
-      <p> Copyright 2024 KanzuCode</p>
-    </div>
+    <div className='footer'>
+        <div className="contact">
+          <h5>Contact Info</h5>
+          <a href="e-mail" type='e-mail' style={{color:'white'}}>ehealthcare@gmail.com</a>
+          <a href="number" style={{color:'white'}}>+256 758903837</a>
+          <a href="number" style={{color:'white'}}>+256 771629949</a>
+        </div>
+        <p> Copyright 2024 KanzuCode</p>
     </div>
   )
 }
