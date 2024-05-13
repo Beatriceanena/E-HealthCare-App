5c18045b0cf7b2dca22fc9c78a88d67d9207bb59
GADOBUA <emmanuelobua54@gmail.com>
2024-05-13 22:52:16 +0300

    make responsive

--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -10,18 +10,18 @@ function Header() {
             <div className="account">
                 <a href="login" style={{color:'black', textDecoration:'none', borderRight:'1px solid black', padding: "5px"}}>Login</a>
                 <a href="account" style={{color:'black', textDecoration:'none'}}>Create Account</a>
-                <button style={{height:'40px', justifyContent:'center', alignItems:'center', backgroundColor:'#089CAD', width:'120px'}}>Book Appointment</button>
+                <button style={{height:'40px', justifyContent:'center', alignItems:'center', backgroundColor:'#089CAD', width:'120px'}}><a href='BookAppointment'> Book Appointment</a></button>
             </div>
             </div>
         <nav>
         
            <ul>
-                <li><a href="links" style={{color:'white', textDecoration:'none'}}>Home</a></li>
-                <li><a href="links" style={{color:'white', textDecoration:'none'}}>Find a Doctor</a></li>
-                <li><a href="links" style={{color:'white', textDecoration:'none'}}>Phamarcy</a></li>
-                <li><a href="links" style={{color:'white', textDecoration:'none'}}>Recources</a></li>
-                <li><a href="links" style={{color:'white', textDecoration:'none'}}>Contact Us</a></li>
-                <li><a href="links" style={{color:'white', textDecoration:'none'}}>Profiles</a></li>
+                <li><a href="Home" style={{color:'white', textDecoration:'none'}}>Home</a></li>
+                <li><a href="Find a Doctor" style={{color:'white', textDecoration:'none'}}>Find a Doctor</a></li>
+                <li><a href="Phamarcy" style={{color:'white', textDecoration:'none'}}>Phamarcy</a></li>
+                <li><a href="Recources" style={{color:'white', textDecoration:'none'}}>Recources</a></li>
+                <li><a href="Contact us" style={{color:'white', textDecoration:'none'}}>Contact Us</a></li>
+                <li><a href="Profiles" style={{color:'white', textDecoration:'none'}}>Profiles</a></li>
                
             </ul>
         </nav>
