003c14e22f163a58fcddd028b6b009b17067b0a1
Anena Beatrice <anenabeattrice@gmail.com>
2024-05-14 13:54:24 +0300

    create pharmacy page

---
+++ src/assets/Components/Product.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div className='product-items'>
+      <div className='cart-item'>
+        <div className='cart-image'>
+          <img src='https://s3.envato.com/files/91718211/Previews/Preview2.jpg' />
+        </div>
+        <div className='item-details'>
+          <h3>Metrohebrophyn</h3>
+          <div className='view'>
+          <p>UGX 200000</p> 
+          <p>View</p>
+          </div>
+          <button id='cart-btn'>Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Product
