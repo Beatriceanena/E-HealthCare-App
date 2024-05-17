8cd826c21a68b25455f60a3430b2a588b0716e30
GADOBUA <emmanuelobua54@gmail.com>
2024-05-17 04:40:24 +0300

    add time schedule options with type of call

--- src/assets/Pages/ScheduleCall.jsx
+++ src/assets/Pages/ScheduleCall.jsx
@@ -8,12 +8,21 @@ function ScheduleCall() {
       <h1>Would you like to schedule a Call with a Doctor</h1>
       <p>Dont worry, we have got you covered. With our 24hour services available. weare sure to get you in touch with an available Doctor in your time of need</p>
       <div className="Schedule">
-        <tr>
-            <th>Date</th>
-            <th>Time</th>
-            <th>Duration</th>
-            <th>Location</th>
-        </tr>
+      <select type="usertype" id="time">
+            <option value="text">Available Slots</option>
+              <option value="time">9:00Am</option>
+              <option value="time">10:00AM</option>
+              <option value="time">10:30AM</option>
+              <option value="time">11:00AM</option>
+              <option value="time">11:30AM</option>
+            </select>
+            <select name="Location" id="Calltype">
+            <option value="text">Location</option>
+            <option value="zoom">Zoom</option>
+            <option value="Google Meet">Google Meet</option>
+            </select>
+          
+            <button>Schedule Call</button>
       </div>
     </div>
   )
