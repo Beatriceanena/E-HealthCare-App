965bb30ca1309ec809d559e14b216815941c6739
GADOBUA <emmanuelobua54@gmail.com>
2024-05-18 13:20:14 +0300

    add the link paths to pages

--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -21,12 +21,20 @@ function Header() {
         <nav>
         
            <ul>
-                <li><a href="Home" style={{color:'white', textDecoration:'none'}}>Home</a></li>
+                        <li><Link to="/Home"  style={{color:'white', textDecoration:'none'}}>Home</Link> </li>
+                        <li><Link to="/Finddoctor" style={{color:'white', textDecoration:'none'}}>Find a  Doctor</Link></li>
+                        <li><Link to="/Pharmacy" style={{color:'white', textDecoration:'none'}}>Pharmacy</Link></li>
+                        <li><Link id='#footer' style={{color:'white', textDecoration:'none'}}>Contact us</Link></li>
+                        <li><Link to="/" style={{color:'white', textDecoration:'none'}}>Profiles</Link></li>
+                        <li><Link to="/Resources" style={{color:'white', textDecoration:'none'}}>Resources</Link> </li>
+
+
+              {/*  <li><a href="Home" style={{color:'white', textDecoration:'none'}}>Home</a></li>
                 <li><a href="Find a Doctor" style={{color:'white', textDecoration:'none'}}>Find a Doctor</a></li>
                 <li><a href="Phamarcy" style={{color:'white', textDecoration:'none'}}>Phamarcy</a></li>
                 <li><a href="Recources" style={{color:'white', textDecoration:'none'}}>Recources</a></li>
                 <li><a href="Contact us" style={{color:'white', textDecoration:'none'}}>Contact Us</a></li>
-                <li><a href="Profiles" style={{color:'white', textDecoration:'none'}}>Profiles</a></li>
+  <li><a href="Profiles" style={{color:'white', textDecoration:'none'}}>Profiles</a></li> */}
                
             </ul>
         </nav>
